f36cac07d8c3f60eb6eb1f9fdc38e4b960e09504
Lavolin <lavolin@gmail.com>
2022-06-26 19:49:31 +0300

    Add project files.

---
+++ Car.Test/CarsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.Test
+{
+    public class CarsTests  
+    {                                           // 1:00
+        [Test]
+        public void Test_Drive_Car_Should_Increase_Mileage()
+        {
+            Car car = new Car();
+            car.Drive(100);
+            
+            Assert.AreEqual(car.Mileage, 100);
+        }
+    }
+}
---
+++ UnitTesting/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car
+{
+    public class Car
+    {
+
+        public int Mileage { get; set; }
+        public void Drive(int kms)
+        {
+            if (kms < 0)
+            {
+                throw new ArgumentException("Ne si dobre!");    
+            }
+            Mileage += kms;
+            
+        }
+    }
+}
