6b2cb0c844377c75ce864869882bf0dbaf0f1050
Todor Stoyanov <lavolin@gmail.com>
2022-06-29 12:43:21 +0300

    29061243

--- DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -6,7 +6,7 @@ namespace DatabaseExtended.Tests
     public class ExtendedDatabaseTests
     {
 
-        [TestCasSource("TestCaseSourceData")]
+        [TestCasSource("TestCaseConstructorData")]
 
         public void Constructor_Should_Create_Database_PositiveTest(
             Person[] people,
@@ -17,16 +17,47 @@ namespace DatabaseExtended.Tests
             Assert.AreEqual(expectedCount, data.Count);
         }
 
-        public static IEnumerable<TestCaseData> TestCaseSourceData()
+        public static IEnumerable<TestCaseData> TestCaseConstructorData()
         {
-            yield return new TestCaseData(
-                new Person[] people 
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                new TestCaseData(
+                new Person[]
                 {
                     new Person(1, "Todor")
                     new Person(2, "Eli")
                    
                 },
-                1);
+                2),
+                new TestCaseData(
+                new Person[0] people 
+                {
+                   
+                },
+                0);
+            }
+
+            foreach (var item in testCases)
+            {
+                yield return item;
+            }
+
+            // yield return new TestCaseData(
+            //     new Person[] people 
+            //     {
+            //         new Person(1, "Todor")
+            //         new Person(2, "Eli")
+                   
+            //     },
+            //     2);
+            //  yield return new TestCaseData(
+            //     new Person[0] people 
+            //     {
+                   
+            //     },
+            //     0);
+
+
         }
     }
 }
