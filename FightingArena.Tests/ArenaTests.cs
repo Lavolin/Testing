d88fe70262847dec3e1c3f8f66b280a6486aa78a
Todor Stoyanov <lavolin@gmail.com>
2022-06-30 12:18:54 +0300

    30061218

--- FightingArena.Tests/ArenaTests.cs
+++ FightingArena.Tests/ArenaTests.cs
@@ -5,6 +5,65 @@
     [TestFixture]
     public class ArenaTests
     {
+        [Test]
+        public void Constructor_Should_Create_DataProperly()
+        {
+            Warrior warrior = new Warrior("Tosho", 100, 100);
 
+            Assert.AreEqual(warrior.Name, "tosho");
+            Assert.AreEqual(warrior.HP, 100);
+            Assert.AreEqual(warrior.Damage, 100);
+
+            Warrior warrior = new Warrior("Tosho", 1, 0);
+
+            Assert.AreEqual(warrior.Name, "tosho");
+            Assert.AreEqual(warrior.HP, 0);
+            Assert.AreEqual(warrior.Damage, 1);
+
+        }
+
+        public void Constructor_Should_ThrowExcepion_On_InvalidData()
+        {
+
+                Assert.Throws<ArgumentException>(
+                () => new Warrior(null, 100, 100));
+
+                 Assert.Throws<ArgumentException>(
+                () => new Warrior("", 100, 100));
+
+                Assert.Throws<ArgumentException>(
+                () => new Warrior(" ", 100, 100));
+
+                Assert.Throws<ArgumentException>(
+                () => new Warrior("Tosho", 0, 100));
+
+                Assert.Throws<ArgumentException>(
+                () => new Warrior("Tosho", -1, 100));
+
+                Assert.Throws<ArgumentException>(
+                () => new Warrior("Tosho", 50, -1));
+
+        }
+
+        public void Fight_ShouldThrowExcepion_InvalidData()
+        {
+            Warrior attacker = new Warrior("Tosho", 50, 5);
+            Warrior defender = new Warrior("Tosho", 50, 50);
+
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(defender));
+
+            Warrior attacker = new Warrior("Tosho", 50, 50);
+            Warrior defender = new Warrior("Tosho", 50, 5);
+
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(defender));
+
+            Warrior attacker = new Warrior("Tosho", 50, 50);
+            Warrior defender = new Warrior("Tosho", 100, 100);
+
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(defender));
+        }
     }
 }
